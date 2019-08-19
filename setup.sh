f248b3c49936cfc9bf9b8ccec23f8e514872e82b
Markus Bühler <dk9mbs@darc.de>
2019-08-19 13:01:38 +0200

    apache

--- setup.sh
+++ setup.sh
@@ -2,3 +2,17 @@
 
 mkdir -p /etc/tuxlog
 cp ./tuxlog_cfg.json /etc/tuxlog/
+
+
+
+
+
+#! /usr/bin/python3.5
+
+import logging
+import sys
+logging.basicConfig(stream=sys.stderr)
+sys.path.insert(0, '/tmp/virtenv/tuxlog/')
+from my_flask_app import app as application
+application.secret_key = 'anything you wish'
+
