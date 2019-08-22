4f636c023f8d85ed130675a753306078aac323e2
Markus Bühler <dk9mbs@darc.de>
2019-08-22 09:30:19 +0200

    devServer Option added

--- vue-ui/vue.config.js
+++ vue-ui/vue.config.js
@@ -1,5 +1,8 @@
 /* https://cli.vuejs.org/guide/webpack.html#modifying-options-of-a-plugin */
 module.exports = {
+devServer: {
+    proxy: 'http://localhost:5000',
+  },
   configureWebpack: {
     resolve: {
     alias: {
