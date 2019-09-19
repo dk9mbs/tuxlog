81ab264ef81cd1fc69b6f0593676f5335cdb76e1
dk9mbs <dk9mbs@heiligenhafen.the-b.eu>
2019-09-19 09:45:18 +0200

    import_adif

--- tools/import-adif.sh
+++ tools/import-adif.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
 IMPORTDIR=$1
-USERNAME=$2
-PASSWORD=$3
-LOGBOOK=$4
+SERVER=$2
+USERNAME=$3
+PASSWORD=$4
+LOGBOOK=$5
 echo "Importdirectory => $IMPORTDIR"
 
 for i in $1/*.adi; do
     echo "importing => $i ..."
-    wget --debug --header "content-type:text/adif" --post-file $i -O - http://$USERNAME:$PASSWORD@localhost:5000/api/v1.0/import/LogLogs?logbook_id=$LOGBOOK
+    wget --debug --header "content-type:text/adif" --post-file $i -O - http://$USERNAME:$PASSWORD@$SERVER/api/v1.0/import/LogLogs?logbook_id=$LOGBOOK >> /tmp/import_adif.log
+    ERRCODE=$?
+
+    if [ $ERRCODE == "0" ] 
+    then
+        echo "Import OK renaming importfile!"
+    	rm $i "$1.imported"
+    fi
+
+
     echo "============================================="
 done
