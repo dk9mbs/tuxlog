b22a15053edeaf94ff1acd3757ebea98984412ae
dk9mbs <dk9mbs@heiligenhafen.the-b.eu>
2019-11-11 20:45:23 +0100

    get dxclsuter from database

--- tuxlog/__solution__/plugins/dxcluster_client.py
+++ tuxlog/__solution__/plugins/dxcluster_client.py
@@ -3,6 +3,8 @@ import logging
 import time
 import threading
 from tuxlog.cluster import ClusterSpot
+from model.model import LogDxclusters, LogDxclusterLoginscripts
+
 logger = logging.getLogger(__name__)
 
 thread=None
@@ -12,10 +14,22 @@ def task():
     import re
     from common.app import socketio
 
-    tn = telnetlib.Telnet("la3waa.ddns.net",8000)
-    #tn =telnetlib.Telnet("157.158.185.199", 9000)
-    tn.read_until(b'login: ') 
-    tn.write(b'DK9MBS \n')
+    cluster=LogDxclusters.get(LogDxclusters.position==0)
+    if cluster==None:
+        logger.info("No defalut DXCluster (position 0) found!")
+        return 
+
+    tn = telnetlib.Telnet(cluster.cluster_url,cluster.port)
+
+    query=LogDxclusterLoginscripts.select().where(LogDxclusterLoginscripts.dxcluster_id==cluster.id)
+    for row in query:
+        logger.info ('loginscript position %s' % row.position)
+        
+        if row.prompt != None:
+            tn.read_until(str.encode(row.prompt))
+
+        logger.info('sending to dxcluster => %s' % row.send)
+        tn.write(str.encode(row.send+'\n'))
 
     while (1):
         spot="".join(map(chr, tn.read_until(b'\n'))).replace('\n','').replace('\r','')
