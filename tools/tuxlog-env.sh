420d8ce24799f0dfabd42009b6caa3260869a0ac
dk9mbs <dk9mbs@heiligenhafen.the-b.eu>
2019-12-24 16:07:05 +0100

    prompt chanched

--- tools/tuxlog-env.sh
+++ tools/tuxlog-env.sh
@@ -2,5 +2,5 @@
 
 ENV=$1
 export TUXLOG_ENVIRONMENT="$ENV"
-export PS1="[tuxlog:$ENV] \[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\u@\h:\w>"
+export PS1="[tuxlog:$ENV] \[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\u@\h:\w > "
 #echo "$ENV environment for tuxlog activated"
